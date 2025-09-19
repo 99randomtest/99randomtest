6dc7cc8c449055eafbbdf1584f154817c0e94896
99randomtest <backupemail99@yahoo.com>
2025-09-18 20:59:25 -0500

    Completed Lab 3.

--- javascript/external.js
+++ javascript/external.js
@@ -1,8 +1,10 @@
-arr = ["Mael","Eric","Randall","James"];
-for (var i=0; i < arr.length; i++)
-{
-document.writeln(arr[i],"<br>");
-}
+// The lab didn't say to delete this part, but the example image
+//we need to match doesn't display these names, so I've hidden them
+// arr = ["Mael","Eric","Randall","James"];
+// for (var i=0; i < arr.length; i++)
+// {
+// document.writeln(arr[i],"<br>");
+// }
 
 user_name = window.prompt("Please enter your name", "Type your name here");
 document.getElementById("username").textContent = user_name;
