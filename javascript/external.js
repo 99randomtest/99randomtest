40e0e12ffc3b9d0294cf05c5a46f16fbda0a4b8e
99randomtest <backupemail99@yahoo.com>
2025-09-25 19:38:28 -0500

    Filled with content from webmap arrays into the webmap table.

--- javascript/external.js
+++ javascript/external.js
@@ -8,13 +8,40 @@
 //user_name = window.prompt("Please enter your name", "Type your name here");
 //document.getElementById("username").textContent = user_name;
 
+var webmaps =
+[
+  ["FEMA National Flood Hazard Layer",
+  "https://hazards-fema.maps.arcgis.com/apps/webappviewer/index.html?id=8b0adb51996444d4879338b5529aa9cd",
+   "The FEMA National Flood Hazard Layer exists to display flood hazard areas and provide an easy source to view and acquire flood hazard layers to use with GIS applications."],
+  ["US Forest Service Webmap",
+  "https://experience.arcgis.com/experience/a695e1dbdeab4e8ba5c6ad4fd06a0ba0",
+  "The purpose of the USFS web map is to provide visitors to National Forests with an interactive map that can display forest areas and attractions like campgrounds or horse-riding trails."]
+];
+
+
 function welcome()
-{
-let a = "Please enter your name.";
-let b = "Type your name here.";
-// A prompt box is used to prompt users to input a value before entering a page.
-user_name = window.prompt(a, b);
-message = "<h1>Hello, welcome to my webpage, " + user_name + "!</h1>"
-return message
-}
+  {
+    let a = "Please enter your name.";
+    let b = "Type your name here.";
+    // A prompt box is used to prompt users to input a value before entering a page.
+    user_name = window.prompt(a, b);
+    message = "<h1>Hello, welcome to my webpage, " + user_name + "!</h1>"
+    return message
+  }
 document.write(welcome());
+
+function webmap_table()
+  {
+    document.write("<table width=100%>");
+    for (var row=0; row < webmaps.length; row++)
+    {
+      document.write("<tr>");
+      for (var column=0; column < webmaps[0].length; column++)
+      {
+        document.write("<td>" + webmaps[row][column] + "</td>");
+      }
+      document.write("</tr>");
+    }
+    document.write("</table>");
+    return "";
+}
